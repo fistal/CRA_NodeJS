a4229c3c312a5454c876f2cf6dec9b96238ab513
Juju <juju@Air-de-Juju.home>
2023-12-01 10:25:19 +0100

    connexion to firebase

--- app.js
+++ app.js
@@ -1,47 +1,55 @@
-const express = require('express'); 
-const { initializeApp } = require('firebase/app');
-const { getFirestore, addDoc, collection } = require('firebase/firestore');
+const express = require("express");
 
 const app = express();
 const port = 3000;
 
-// Chemin du fichier JSON téléchargé
-const serviceAccount = require('./cranodejs-406800-f6bb8a8db8b5.json');
+const {
+  initializeApp,
+  applicationDefault,
+  cert,
+} = require("firebase-admin/app");
+const {
+  getFirestore,
+  Timestamp,
+  FieldValue,
+  Filter,
+} = require("firebase-admin/firestore");
 
-// Configuration Firebase
-const firebaseConfig = {
-  apiKey: 'AIzaSyBN-RCoOPoZx_ONIPhYg9ya-oqY1Pd811Q',
-  authDomain: 'cranodejs.firebaseapp.com',
-  projectId: 'cranodejs',
-  storageBucket: 'cranodejs.appspot.com',
-  messagingSenderId: '528675429664',
-  appId: '1:528675429664:web:e187c31ddde2309cb08b45',
-};
+const serviceAccount = require("./cranodejs-406800-f6bb8a8db8b5.json");
 
-const firebaseApp = initializeApp(firebaseConfig, {useFetchStreams: false})
-const db = getFirestore(firebaseApp, { keyFilename: serviceAccount });
+initializeApp({
+  credential: cert(serviceAccount),
+});
+
+const db = getFirestore();
 
 // Middleware pour les requêtes JSON et URL encodées
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Route principale
-app.get('/', (req, res) => {
-  res.send('<h1>Bienvenue sur votre application légère !</h1>');
+app.get("/", (req, res) => {
+  res.send("<h1>Bienvenue sur votre application légère !</h1>");
 });
 
 // Route pour afficher le formulaire
-app.get('/ajouter-intervention', (req, res) => {
-  res.sendFile(__dirname + '/views/formulaire.html');
+app.get("/ajouter-intervention", (req, res) => {
+  res.sendFile(__dirname + "/views/formulaire.html");
 });
 
 // Route pour traiter le formulaire
-app.post('/ajouter-intervention', async (req, res) => {
+app.post("/ajouter-intervention", async (req, res) => {
   try {
-    const { dateIntervention, mission, ressource, charge, statut, validationClient } = req.body;
+    const {
+      dateIntervention,
+      mission,
+      ressource,
+      charge,
+      statut,
+      validationClient,
+    } = req.body;
 
-    // Ajouter les données à Firestore
-    const docRef = await addDoc(collection(db, 'interventions'), {
+    const newIntervention = await db.collection("interventions").add({
       dateIntervention,
       mission,
       ressource,
@@ -50,19 +58,19 @@ app.post('/ajouter-intervention', async (req, res) => {
       validationClient,
     });
 
-    console.log('Document ajouté avec ID :', docRef.id);
+    console.log("Added document with ID: ", newIntervention.id);
 
     // Redirigez l'utilisateur vers une page de confirmation
-    res.redirect('/confirmation');
+    res.redirect("/confirmation");
   } catch (err) {
-    console.error('Erreur lors de l\'ajout des données à Firestore:', err);
-    res.status(500).send('Erreur lors de l\'ajout des données à Firestore.');
+    console.error("Erreur lors de l'ajout des données à Firestore:", err);
+    res.status(500).send("Erreur lors de l'ajout des données à Firestore.");
   }
 });
 
 // Route pour la page de confirmation
-app.get('/confirmation', (req, res) => {
-  res.send('<h2>Données enregistrées avec succès !</h2>');
+app.get("/confirmation", (req, res) => {
+  res.send("<h2>Données enregistrées avec succès !</h2>");
 });
 
 // Démarrer le serveur
